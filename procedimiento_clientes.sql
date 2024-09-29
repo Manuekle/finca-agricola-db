20a98c8faa43bb9a42fecaeac8491b7bebef061e
Iver Mendez <iverarleyb@gmail.com>
2024-09-29 08:55:37 -0500

    Se creo el procedimiento almacenado de la tabla CLIENTES

---
+++ procedimiento_clientes.sql
@@ -0,0 +1,47 @@
+-- INSERT
+DELIMITER //
+CREATE PROCEDURE sp_insert_clientes(
+    IN v_clie_nombre VARCHAR(100),
+    IN v_clie_telefono VARCHAR(15),
+    IN v_clie_direccion VARCHAR(255)
+)
+BEGIN
+    INSERT INTO clientes (clie_nombre, clie_telefono, clie_direccion)
+    VALUES (v_clie_nombre, v_clie_telefono, v_clie_direccion);
+END//
+DELIMITER ;
+
+-- UPDATE
+DELIMITER //
+CREATE PROCEDURE sp_update_clientes(
+    IN v_clie_id INT,
+    IN v_clie_nombre VARCHAR(100),
+    IN v_clie_telefono VARCHAR(15),
+    IN v_clie_direccion VARCHAR(255)
+)
+BEGIN
+    UPDATE clientes
+    SET clie_nombre = v_clie_nombre, 
+        clie_telefono = v_clie_telefono, 
+        clie_direccion = v_clie_direccion
+    WHERE clie_id = v_clie_id;
+END//
+DELIMITER ;
+
+-- SHOW
+DELIMITER //
+CREATE PROCEDURE sp_show_clientes()
+BEGIN
+    SELECT clie_id, clie_nombre, clie_telefono, clie_direccion FROM clientes;
+END//
+DELIMITER ;
+
+-- DELETE
+DELIMITER //
+CREATE PROCEDURE sp_delete_clientes(
+    IN v_clie_id INT
+)
+BEGIN
+    DELETE FROM clientes WHERE clie_id = v_clie_id;
+END//
+DELIMITER ;
