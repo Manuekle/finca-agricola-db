9afe11e373abbc2a3c6b84ea64b1fdf35410d444
IverArley <105886427+IverArley@users.noreply.github.com>
2024-11-13 15:07:46 -0500

    Update procedimiento_detalle_venta.sql

--- procedimiento_detalle_venta.sql
+++ procedimiento_detalle_venta.sql
@@ -1,50 +1,46 @@
 -- INSERT
 DELIMITER //
-CREATE PROCEDURE sp_insert_detalle_venta(
-    IN v_ven_id INT,
+CREATE PROCEDURE sp_insert_detalle_ventas(
+    IN v_vent_id INT,
     IN v_prod_id INT,
-    IN v_det_cantidad DECIMAL(10,2),
-    IN v_det_precio DECIMAL(10,2)
+    IN v_deta_cantidad INT
 )
 BEGIN
-    INSERT INTO detalle_venta (ven_id, prod_id, det_cantidad, det_precio)
-    VALUES (v_ven_id, v_prod_id, v_det_cantidad, v_det_precio);
-END//
-DELIMITER ;
-
--- SHOW
-DELIMITER //
-CREATE PROCEDURE sp_show_detalle_venta()
-BEGIN
-    SELECT * FROM detalle_venta;
+    INSERT INTO detalle_ventas (vent_id, prod_id, deta_cantidad)
+    VALUES (v_vent_id, v_prod_id, v_deta_cantidad);
 END//
 DELIMITER ;
 
 -- UPDATE
 DELIMITER //
-CREATE PROCEDURE sp_update_detalle_venta(
-    IN v_det_id INT,
-    IN v_ven_id INT,
+CREATE PROCEDURE sp_update_detalle_ventas(
+    IN v_vent_id INT,
     IN v_prod_id INT,
-    IN v_det_cantidad DECIMAL(10,2),
-    IN v_det_precio DECIMAL(10,2)
+    IN v_deta_cantidad INT
 )
 BEGIN
-    UPDATE detalle_venta
-    SET ven_id = v_ven_id,
-        prod_id = v_prod_id,
-        det_cantidad = v_det_cantidad,
-        det_precio = v_det_precio
-    WHERE det_id = v_det_id;
+    UPDATE detalle_ventas
+    SET deta_cantidad = v_deta_cantidad
+    WHERE vent_id = v_vent_id AND prod_id = v_prod_id;
+END//
+DELIMITER ;
+
+-- SHOW
+DELIMITER //
+CREATE PROCEDURE sp_show_detalle_ventas()
+BEGIN
+    SELECT vent_id, prod_id, deta_cantidad FROM detalle_ventas;
 END//
 DELIMITER ;
 
 -- DELETE
 DELIMITER //
-CREATE PROCEDURE sp_delete_detalle_venta(
-    IN v_det_id INT
+CREATE PROCEDURE sp_delete_detalle_ventas(
+    IN v_vent_id INT,
+    IN v_prod_id INT
 )
 BEGIN
-    DELETE FROM detalle_venta WHERE det_id = v_det_id;
+    DELETE FROM detalle_ventas WHERE vent_id = v_vent_id AND prod_id = v_prod_id;
 END//
 DELIMITER ;
+
