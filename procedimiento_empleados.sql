9b3be9a82d37fc961a380a5d6a3a58dd09a4dff6
Iver Mendez <iverarleyb@gmail.com>
2024-11-13 11:03:58 -0500

    se corrigue el sp de empleados

--- procedimiento_empleados.sql
+++ procedimiento_empleados.sql
@@ -1,14 +1,33 @@
 -- INSERT
 DELIMITER //
 CREATE PROCEDURE sp_insert_empleados(
-    IN v_emp_nombre VARCHAR(100),
-    IN v_emp_cargo VARCHAR(100),
-    IN v_emp_contacto VARCHAR(255),
-    IN v_par_id INT
+    IN v_empl_nombre VARCHAR(100),
+    IN v_empl_rol VARCHAR(50),
+    IN v_empl_fecha_contratacion DATE,
+    IN v_parc_id INT
 )
 BEGIN
-    INSERT INTO empleados (emp_nombre, emp_cargo, emp_contacto, par_id)
-    VALUES (v_emp_nombre, v_emp_cargo, v_emp_contacto, v_par_id);
+    INSERT INTO empleados (empl_nombre, empl_rol, empl_fecha_contratacion, parc_id)
+    VALUES (v_empl_nombre, v_empl_rol, v_empl_fecha_contratacion, v_parc_id);
+END//
+DELIMITER ;
+
+-- UPDATE
+DELIMITER //
+CREATE PROCEDURE sp_update_empleados(
+    IN v_empl_id INT,
+    IN v_empl_nombre VARCHAR(100),
+    IN v_empl_rol VARCHAR(50),
+    IN v_empl_fecha_contratacion DATE,
+    IN v_parc_id INT
+)
+BEGIN
+    UPDATE empleados
+    SET empl_nombre = v_empl_nombre, 
+        empl_rol = v_empl_rol, 
+        empl_fecha_contratacion = v_empl_fecha_contratacion,
+        parc_id = v_parc_id
+    WHERE empl_id = v_empl_id;
 END//
 DELIMITER ;
 
@@ -16,35 +35,17 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE sp_show_empleados()
 BEGIN
-    SELECT * FROM empleados;
-END//
-DELIMITER ;
-
--- UPDATE
-DELIMITER //
-CREATE PROCEDURE sp_update_empleados(
-    IN v_emp_id INT,
-    IN v_emp_nombre VARCHAR(100),
-    IN v_emp_cargo VARCHAR(100),
-    IN v_emp_contacto VARCHAR(255),
-    IN v_par_id INT
-)
-BEGIN
-    UPDATE empleados
-    SET emp_nombre = v_emp_nombre, 
-        emp_cargo = v_emp_cargo,
-        emp_contacto = v_emp_contacto,
-        par_id = v_par_id
-    WHERE emp_id = v_emp_id;
+    SELECT empl_id, empl_nombre, empl_rol, empl_fecha_contratacion, parc_id FROM empleados;
 END//
 DELIMITER ;
 
 -- DELETE
 DELIMITER //
 CREATE PROCEDURE sp_delete_empleados(
-    IN v_emp_id INT
+    IN v_empl_id INT
 )
 BEGIN
-    DELETE FROM empleados WHERE emp_id = v_emp_id;
+    DELETE FROM empleados WHERE empl_id = v_empl_id;
 END//
 DELIMITER ;
+
